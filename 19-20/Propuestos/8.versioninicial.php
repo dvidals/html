e5d9ad07426c5beb601014494c913b8294e5e1c1
dvidals <35974623+dvidals@users.noreply.github.com>
2019-11-06 12:50:33 +0100

    Create 8.versioninicial.php

---
+++ 19-20/Propuestos/8.versioninicial.php
@@ -0,0 +1,28 @@
+<?php
+
+# Escribe un programa que ordene números enteros introducidos por teclado.
+$a=array(1,2,3,4,69,125,14,33,48,12);
+function ordenar ($a){
+   for($j=0;$j<count($a);$j++){
+    for ($i=0;$i<count($a);$i++){
+        if ($a[$i]==max($a)){
+            $contenedor[$j]=$a[$i];
+            $a[$i]="";
+         // for($i=0;$i<count($a);$i++)echo $a[$i]; //lleva implicito un break en la practica, por eso funcionaba cuando no teníamos el break.
+            break; // si le ponemos un break funciona, si no se lo ponemos el if se ejecuta dos veces en vez de una en alguna de las iteraciones
+        }
+        
+    }
+     echo "<br>";  
+     //echo $contenedor[$j]."<br>";
+    }
+  echo count($contenedor)."<br>";
+  foreach ($contenedor as $value) {
+       echo $contenedor=$value."<br>";
+        
+    } 
+}
+
+echo ordenar($a);
+
+?>
