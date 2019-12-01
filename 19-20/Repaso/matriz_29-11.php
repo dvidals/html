952565e52015b4d02ea1e5a4b86c34d613926d7f
dvidals <35974623+dvidals@users.noreply.github.com>
2019-12-01 10:14:36 +0100

    Update matriz_29-11.php

--- 19-20/Repaso/matriz_29-11.php
+++ 19-20/Repaso/matriz_29-11.php
@@ -43,4 +43,20 @@ define ("m",8);
      }
      echo "<br/>";
  }
+echo "<br/><br/>";
+  
+ foreach ($a as $c1=> $v1){
+     foreach ($v1 as $c2=> $v2){
+         if($v2==0){ 
+            if(($a[$c1][$c2-1]==1 and $c2>0) or ($a[$c1][$c2+1]==1 and $c2<m-1) or ($a[$c1-1][$c2]==1 and $c1>0) or ($a[$c1+1][$c2]==1 and $c1<n-1)
+                    or ($a[$c1-1][$c2-1]==1 and ($c1>0 and $c2>0)) or ($a[$c1+1][$c2-1]==1 and ($c1<n-1 and $c2>0))
+                    or ($a[$c1-1][$c2-1]==1 and ($c1>0 and $c2<m-1)) or ($a[$c1-1][$c2-1]==1 and ($c1<n-1 and $c2<m-1))){
+                echo "<b>".$v2."</b>";
+            }
+         else echo "$v2";
+         }
+         else echo "$v2";
+     }
+     echo "<br/>";
+ }
  
