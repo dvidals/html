c4f43f36c9b59931cdc7fd3f5aaf388af09fc93f
dvidals <35974623+dvidals@users.noreply.github.com>
2019-12-04 14:15:50 +0100

    Add files via upload

---
+++ 19-20/Examen1/funciones.php
@@ -0,0 +1,27 @@
+<?php
+
+function CrearDNI (){
+    $array=array();
+    $numero=array();
+    $letra="";
+    $dni="";
+    for ($i=0;$i<8;$i++){
+        $numero[$i]=rand(0,9);
+    }
+    $numero=implode("",$numero);
+    
+    $letra=substr("TRWAGMYFPDXBNJZSQVHLCKE", $numero % 23, 1);
+    $dni=$numero.$letra;
+   return $dni;
+     
+}
+
+function CrearMatriz($n,$m){
+    $a=array();
+    for ($i=0;$i<$n;$i++){
+        for($j=0;$j<$m;$j++){
+            $a[$i][$j]=CrearDNI();
+        }
+    }
+    return $a;
+}
