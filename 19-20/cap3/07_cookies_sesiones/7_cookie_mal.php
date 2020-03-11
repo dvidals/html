01d72eec1a8759a608fa5484c2e614bca9df9490
dvidals <despedidachicho@hotmail.com>
2020-03-11 17:21:44 +0100

    ok

--- 19-20/cap3/07_cookies_sesiones/7_cookie_mal.php
+++ 19-20/cap3/07_cookies_sesiones/7_cookie_mal.php
@@ -1,5 +1,9 @@
 <?php
 
 setcookie('nueva', "valor", time() + 3600 * 24);
-echo $_COOKIES["nueva"];
+echo $_COOKIE["nueva"]; //Las variables superglobales se cargan con lo que tengan almacenado antes de escribir ningún código. Por lo que la 
+//primera vez $_COOKIE no tiene nada, la segunda vez que se carga la página ya si porque ahora ya tiene almacenado lo que
+// se le cargo con la instrucción de setcookie
+var_dump($_COOKIE);
 
+//arrays con objetos, estudiar el primer parcial.
